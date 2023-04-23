a476dbab640aeca7c955f8e76a140e95a56229ab
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:15:01 +0600

    deleted: yarn.lock

---
+++ components/layouts/MainHeader.jsx
@@ -0,0 +1,22 @@
+import { Container, Text, Flex, Header} from '@mantine/core';
+import Link from 'next/link';
+import { Logo } from '../ui/Logo';
+import AuthBtn from '../auth/AuthBtn';
+
+export default function MainHeader() {
+  return (
+    <Header height={60} pt={5}>
+      <Container size="lg" className="w-full flex justify-between items-center">
+        <Flex gap="lg" align="center">
+          <Logo />
+          <Link href="/team">
+            <Text color="dimmed" className="cursor-pointer">Team</Text>
+          </Link>
+        </Flex>
+        <Flex gap="md">
+          <AuthBtn />
+        </Flex>
+      </Container>
+    </Header>
+  );
+}
