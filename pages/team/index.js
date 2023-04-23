c050f766988ddfde07030d23b725f23880dc41bb
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:06:52 +0600

    created: Team page

---
+++ pages/team/index.js
@@ -0,0 +1,13 @@
+import { Container } from '@mantine/core';
+import MainHeader from "../../components/layouts/MainHeader";
+
+export default function TeamPage() {
+  return (
+    <>
+      <MainHeader />
+      <Container size="lg">
+        Team
+      </Container>
+    </>
+  );
+}
