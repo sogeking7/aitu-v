7b5ccfd76b0ccc5abd4f88019e85fee8c876f95d
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:37:50 +0600

    fixed: refactored to tsx

--- components/layouts/MainHeader.tsx
+++ components/layouts/MainHeader.tsx
@@ -1,15 +1,15 @@
-import { Container, Text, Flex, Header} from '@mantine/core';
+import { Container, Text, Flex, Header } from '@mantine/core';
 import Link from 'next/link';
 import { Logo } from '../ui/Logo';
-import AuthBtn from '../auth/AuthBtn';
+import { AuthBtn } from '../auth/AuthBtn';
 
-export default function MainHeader() {
+export const MainHeader = () => {
   return (
     <Header height={60} pt={5}>
       <Container size="lg" className="w-full flex justify-between items-center">
         <Flex gap="lg" align="center">
           <Logo />
-          <Link href="/team">
+          <Link href="/team" passHref>
             <Text color="dimmed" className="cursor-pointer">Team</Text>
           </Link>
         </Flex>
@@ -19,4 +19,4 @@ export default function MainHeader() {
       </Container>
     </Header>
   );
-}
+};
