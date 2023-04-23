a6398e517b8fd16b23b19e64c8942b612bf60b09
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:38:34 +0600

    fixed: refactored to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import {Container} from '@mantine/core';
-import MainHeader from '../components/layouts/MainHeader';
+import { Container } from '@mantine/core';
+import { MainHeader } from '../components/layouts/MainHeader';
 
 export default function HomePage() {
   return (
     <>
-      <MainHeader/>
+      <MainHeader />
       <Container size="lg">
         Main
       </Container>
