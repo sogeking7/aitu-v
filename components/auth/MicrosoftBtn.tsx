631c21e2b146fe5ab0c60bf72635e1bbf4d2cb0b
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:35:41 +0600

    fixed: refactored to tsx

---
+++ components/auth/MicrosoftBtn.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@mantine/core';
+import { signIn } from 'next-auth/react';
+
+export const MicrosoftBtn = () => (
+    <Button onClick={() => signIn} w="100%" variant="default" size="md" radius="xl">
+      Sign in with Microsoft
+    </Button>
+);
