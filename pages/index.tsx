4a49858342cb3b2c3f10d01eb941ba042083516f
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 04:14:01 +0600

    fixed: protected with middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,44 @@
-import { Container } from '@mantine/core';
+import { getServerSession } from 'next-auth';
+import { Container, Text } from '@mantine/core';
+import { getProviders, useSession } from 'next-auth/react';
 import { MainHeader } from '../components/layouts/MainHeader';
+import { authOptions } from './api/auth/[...nextauth]';
 
 export default function HomePage() {
-  return (
-    <>
-      <MainHeader />
-      <Container size="lg">
-        Main
-      </Container>
-    </>
-  );
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                <MainHeader />
+                <Container size="lg" className="flex justify-center" py={100}>
+                    Home Page
+                </Container>
+            </>
+        );
+    }
+    return <Text>Access Denied</Text>;
+}
+
+export async function getServerSideProps(context:any) {
+    const session = await getServerSession(context.req, context.res, authOptions);
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/signin',
+                permanent: false,
+            },
+        };
+    }
+
+    const providers = await getProviders();
+    return {
+        props: {
+            providers,
+            session: await getServerSession(
+                context.req,
+                context.res,
+                authOptions
+            ),
+        },
+    };
 }
--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,13 +1,44 @@
-import { Container } from '@mantine/core';
+import { Container, Text } from '@mantine/core';
+import { getServerSession } from 'next-auth';
+import { getProviders, useSession } from 'next-auth/react';
 import { MainHeader } from '../../components/layouts/MainHeader';
+import { authOptions } from '../api/auth/[...nextauth]';
 
 export default function TeamPage() {
-  return (
-    <>
-      <MainHeader />
-      <Container size="lg">
-        Team
-      </Container>
-    </>
-  );
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                <MainHeader />
+                <Container size="lg" className="flex justify-center" py={100}>
+                    Team Page
+                </Container>
+            </>
+        );
+    }
+    return <Text>Access Denied</Text>;
+}
+
+export async function getServerSideProps(context:any) {
+    const session = await getServerSession(context.req, context.res, authOptions);
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth/signin',
+                permanent: false,
+            },
+        };
+    }
+
+    const providers = await getProviders();
+    return {
+        props: {
+            providers,
+            session: await getServerSession(
+                context.req,
+                context.res,
+                authOptions
+            ),
+        },
+    };
 }
