127d5007cb0835e3cc2d217b833f094097dd8f83
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 00:06:40 +0600

    created: Main page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import {Container} from '@mantine/core';
+import MainHeader from '../components/layouts/MainHeader';
+
+export default function HomePage() {
+  return (
+    <>
+      <MainHeader/>
+      <Container size="lg">
+        Main
+      </Container>
+    </>
+  );
+}
