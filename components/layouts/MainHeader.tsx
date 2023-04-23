0f0fbe7eebe48df7244ec06b188b86121cd8c816
Kaiyrkhan <kairolla222@gmail.com>
2023-04-24 04:13:40 +0600

    added: creatStyles

--- components/layouts/MainHeader.tsx
+++ components/layouts/MainHeader.tsx
@@ -1,16 +1,23 @@
-import { Container, Text, Flex, Header } from '@mantine/core';
+import { Container, Text, Flex, Header, createStyles } from '@mantine/core';
 import Link from 'next/link';
 import { Logo } from '../ui/Logo';
 import { AuthBtn } from '../auth/AuthBtn';
 
+const useStyles = createStyles(() => ({
+  link: {
+    textDecoration: 'none',
+  },
+}));
+
 export const MainHeader = () => {
+  const { classes } = useStyles();
   return (
     <Header height={60} pt={5}>
       <Container size="lg" className="w-full flex justify-between items-center">
         <Flex gap="lg" align="center">
           <Logo />
-          <Link href="/index" passHref>
-            <Text color="dimmed" className="cursor-pointer">Team</Text>
+          <Link href="/team" passHref className={classes.link}>
+            <Text color="dimmed">Team</Text>
           </Link>
         </Flex>
         <Flex gap="md">
